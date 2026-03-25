c0bf4da5dbe98509a32d8767d3dd86b26be511fc
Sathwik <sathwik5tallapaka@gmail.com>
2026-03-25 14:40:11 +0530

    updated

--- app/src/main/java/com/simats/interviewassist/data/network/RetrofitClient.kt
+++ app/src/main/java/com/simats/interviewassist/data/network/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    const val BASE_URL = "http://172.22.207.190:5000/"
+    const val BASE_URL = "http://10.12.215.190:5000/"
     private var authToken: String? = null
 
     fun setAuthToken(token: String?) {
